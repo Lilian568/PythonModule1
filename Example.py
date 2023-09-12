4f1d99ce0f9a8d1af3d4c517d6a919aab189666a
phuonglt568 <phuonglinh08625@gmail.com>
2023-09-12 19:22:32 +0300

    update the module 6

--- Example.py
+++ Example.py
@@ -63,7 +63,7 @@ num1 = int(input("give me the 1st number: "))
 num2 = int(input("give me the 2nd number: "))
 addition = calculation(num1, num2)
 print ("the sum of these two numbers is: ", addition)
-"""
+
 def print_spruce(size):
     print("A spruce is coming!")
     for i in range(1, size + 1):
@@ -72,3 +72,84 @@ def print_spruce(size):
         print(space + stars)
 print_spruce(6)
 
+ # Session 6
+ myTuples = (1,35,5)
+ for i in myTuples:
+     print(i)
+point = (2,4)
+x,y = point
+print(point)
+print(f"here is the x {x} value")
+student = ("Linh", 20, "Computer science")
+print(f"Name: {student[0]}, Age:{student[1], Major{student[2]}")
+
+
+grades = (6,8,9,10)
+total = 0
+for grade in grades:
+    total +=grade
+print(f"Here is the total{total}: ")
+
+students = []
+students.append(("Linh", (60,70,80,100,40)))
+students.append(("Son", (50,70,60,90,100)))
+students.append(("Khanh", (80,90,70,80,100)))
+found = False
+print(students)
+for student in students:
+    if student[0] == "Linh":
+        print(f"the name, {students[0][0]}")
+        print(f"the grades, {students[0][1]}")
+        avg = sum(student[1])/ len(student[1])
+        print(f"The average of the {student[0]},  is {avg}")
+
+        found = True
+        break
+    if not found:
+        print("Invalid name")
+student.append("Divid")
+
+student = {
+    "name": "Amir",
+    "grade": "A",
+    "courses":["Math","Physics", "Programming"]
+}
+print("Name: ", student["name"])
+print("grade", student["grade"])
+print("courses", student["courses"])
+
+student["age"] = 21
+print("Age ", student["age"])
+
+student["city"] = "ESPOO"
+student["age"] = 21
+print(student)
+for key, value in student.items():
+    print(key + " :", value)
+
+if "age" in student:
+    print("age: ", student["age"])
+else:
+    print("not in the dictionary")
+
+"""
+shopping_list = {
+    "milk": 4,
+    "juice": 3,
+    "apple": 3,
+    "oat": 2
+}
+def add_item_to_shopping_list (shopping_list, item, quantity):
+    if item in shopping_list:
+        shopping_list[item] += quantity
+    else:
+        shopping_list[item] = quantity
+add_item_to_shopping_list (shopping_list,"egg", 5 )
+def display_shopping_list(shopping_list):
+    total = 0
+    print("Shopping List:")
+    for item, quantity in shopping_list.items():
+        print(f"{item}:{quantity}")
+        total += quantity
+    print(f"total items: {total}")
+display_shopping_list (shopping_list)
---
+++ PythonModule6.py
@@ -0,0 +1,82 @@
+import random
+import math
+"""
+#Ex6.1
+def roll_dice():
+    return random.randint(1, 6)
+def main():
+    rolls = 0
+    while True:
+        result = roll_dice()
+        rolls += 1
+        print(f"roll {rolls}: {result}")
+        if result == 6:
+            break
+if __name__ == "__main__":
+    main()
+
+#Ex6.2
+def roll_dice(sides):
+    return random.randint(1, sides)
+def main():
+    max_sides = int(input("Enter the maximum number of sides on the dice:"))
+    rolls = 0
+    while True:
+        result = roll_dice(max_sides)
+        rolls += 1
+        print(f"Roll {rolls}:{result}")
+        if result ==max_sides:
+            break
+if __name__ == "__main__":
+    main()
+
+#Ex6.3
+def volume_liters(x):
+    liters = gallon * 3.7854
+    return liters
+while True:
+    gallon = int(input("Enter volume in gallon: "))
+    result = volume_liters(gallon)
+    if result > 0:
+        print(f"the volume converts from {gallon} gallon to liters is {result} liters")
+    else:
+        result < 0
+        print(f"Invalid value")
+        break
+
+#Ex6.4
+def sum_of_number(list):
+    total = 0
+    for x in list:
+        total = total + x
+    return total
+if __name__ == "__main__":
+    list_of_integer = (1,2,3,4,5,6,7,8,9,10)
+    result = sum_of_number(integer_list)
+    print(f"The sum of list of integer is:{result}")
+    
+    
+#Ex6.5
+def remove_odd_numbers(numbers):
+    even_numbers = []
+    for num in numbers:
+        if num % 2 == 0:
+            even_numbers.append(num)
+    return even_numbers
+if __name__ == "__main__":
+    integer_list = [1,2,3,4,5,6,7,8,9,10]
+    even_list = remove_odd_numbers(integer_list)
+    print("Original list:", integer_list)
+    print("Cut down list(even numbers only):", even_list)
+"""
+#Ex6.6
+def calculate_unit_price(diameter_cm, price_euros):
+    diameter_m = diameter_cm / 100
+    area_sqm = math.pi * (diameter_m / 2) ** 2
+    unit_price_per_sqm = price_euros / area_sqm
+    return unit_price_per_sqm
+print("Welcome to the Pizza Calculator!")
+diameter1 = float(input("Enter the diameter of the first pizza: "))
+price1 = float(input("Enter the price of the first pizza"))
+diameter2 = float(input("Enter the diameter of the second pizza: "))
+price2 = float(input("Enter the diameter of the second pizza: "))
